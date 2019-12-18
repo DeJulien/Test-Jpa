255fcdd05f0505db10f6c788f95f63382a40a690
Julien Dewost <dewostjulien@gmail.com>
2019-12-18 11:32:09 +0100

    tp1 test

--- src/main/java/fr/diginamic/jpa/test/TestJpa.java
+++ src/main/java/fr/diginamic/jpa/test/TestJpa.java
@@ -1,10 +1,24 @@
 package fr.diginamic.jpa.test;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class TestJpa {
 
 	public static void main(String[] args) {
-		
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("compta");
+		EntityManager em = entityManagerFactory.createEntityManager();
+		System.out.println(em);
+		Fournisseur fourni = em.find(Fournisseur.class, 1);
+		if (fourni != null){
+			System.out.println(fourni.getId()+" "+fourni.getNom());
+		}
 
+
+
+		em.close();
+		entityManagerFactory.close();
 	}
 
 }
